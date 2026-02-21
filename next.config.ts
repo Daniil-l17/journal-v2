6ea3fa46cf9497f8db72d06449305e03b0a56b27
Daniil-l17 <129774580+Daniil-l17@users.noreply.github.com>
2026-02-21 20:39:35 +0500

    Update next.config.ts to remove outdated i18n comment and add react-intl dependency in package.json and package-lock.json for internationalization support.

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,5 @@
 import type { NextConfig } from 'next'
 
-// i18n в next.config работает только с Pages Router. В App Router маршруты с [locale] + middleware.
 const nextConfig: NextConfig = {}
 
 export default nextConfig
